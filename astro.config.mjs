b2e608468f61c32aceec4e0cf71e15579060b514
adriperezz <127222542+adriperezz@users.noreply.github.com>
2025-04-04 13:22:50 +0200

    Testing tag manager

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import react from '@astrojs/react';
 
 import sitemap from '@astrojs/sitemap';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), react(), sitemap()],
+  integrations: [tailwind(), icon(), react(), sitemap(), partytown()],
   devToolbar: {
     enabled: false,
   },
